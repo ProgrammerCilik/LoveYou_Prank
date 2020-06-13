009e450d3d0600722996607a726af73094301562
AkhmadZiyad <64240233+ProgrammerCilik@users.noreply.github.com>
2020-06-13 13:48:29 +0700

    Add files via upload

---
+++ LoveYou_Prank.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#LoveYou Script!
+echo "====================="
+echo "= Siapa Nama Kakak? ="
+echo "====================="
+read Nama
+echo "====================================="
+echo "= I Love You $Nama.. If You Love Me ="
+echo "= I Love You To.. Hehehehehehe...   ="
+echo "====================================="
+exit
